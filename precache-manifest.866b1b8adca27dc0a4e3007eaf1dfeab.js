5711d4e6eb2eea635cc967b123c8584723235c1b
Tanmoy Das <tanmoydas1997@gmail.com>
2021-01-12 23:57:25 +0530

    Updates

---
+++ precache-manifest.866b1b8adca27dc0a4e3007eaf1dfeab.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8e644076f5f3b475dc4771ad815d81",
+    "url": "/Reactgram/index.html"
+  },
+  {
+    "revision": "b5da41cd6245a7338853",
+    "url": "/Reactgram/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "d061e817a300bade8efd",
+    "url": "/Reactgram/static/css/3.75b88c9f.chunk.css"
+  },
+  {
+    "revision": "1d7d597cbe42a70e9e28",
+    "url": "/Reactgram/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "4b07edb0417753fc24be",
+    "url": "/Reactgram/static/css/7.792a87e0.chunk.css"
+  },
+  {
+    "revision": "55129daf1ffbd946a729",
+    "url": "/Reactgram/static/css/main.912673cd.chunk.css"
+  },
+  {
+    "revision": "b5da41cd6245a7338853",
+    "url": "/Reactgram/static/js/0.1ba12204.chunk.js"
+  },
+  {
+    "revision": "d061e817a300bade8efd",
+    "url": "/Reactgram/static/js/3.54311c1b.chunk.js"
+  },
+  {
+    "revision": "f744bcd552d71ee429c8",
+    "url": "/Reactgram/static/js/4.9c7ec145.chunk.js"
+  },
+  {
+    "revision": "1d7d597cbe42a70e9e28",
+    "url": "/Reactgram/static/js/5.1b79a16b.chunk.js"
+  },
+  {
+    "revision": "18bf1093b6e383c286e1",
+    "url": "/Reactgram/static/js/6.1581c2ed.chunk.js"
+  },
+  {
+    "revision": "4b07edb0417753fc24be",
+    "url": "/Reactgram/static/js/7.befae61c.chunk.js"
+  },
+  {
+    "revision": "55129daf1ffbd946a729",
+    "url": "/Reactgram/static/js/main.44fb86c3.chunk.js"
+  },
+  {
+    "revision": "3b9f4a7cb4cbb6d186c0",
+    "url": "/Reactgram/static/js/runtime~main.2f533a67.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/Reactgram/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/Reactgram/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/Reactgram/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/Reactgram/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
